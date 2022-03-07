67b125a1d440dd90bda077a854574dc5f8f7c598
omeranha <53233626+omeranha@users.noreply.github.com>
2022-03-07 20:03:43 -0300

    fixes and error messages

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,46 @@
 import * as vscode from "vscode";
 import * as settings from "./settings";
+import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand("run-canary", () => {
-		if (!settings.getCanaryPath()) {
-			vscode.window.showErrorMessage('Canary/Datapack path is missing.');
+		if (settings.getCanaryPath() === "" || settings.getBuildPath() === "") {
+			vscode.window.showErrorMessage('Canary or build path is missing.');
 			return;
 		}
 
-		let terminal: vscode.Terminal | undefined;
-		terminal = terminal || vscode.window.createTerminal("Canary");
-		terminal.show();
-		terminal.sendText(`xcopy ${settings.getCanaryPath()} ${settings.getBuildPath()} /s /y`);
-		terminal.sendText(`cd ${settings.getCanaryPath()}`);
-		terminal.sendText(`./canary.exe`);
+		if (!fs.existsSync(`${settings.getCanaryPath()}/canary.exe`)) {
+			vscode.window.showErrorMessage('canary.exe is missing.');
+			return;
+		}
+
+		let canary: vscode.Terminal | undefined;
+		canary = canary || vscode.window.createTerminal("Canary");
+		canary.show();
+		canary.sendText(`robocopy ${settings.getCanaryPath()} ${settings.getBuildPath()} /nfl /ndl /njh /njs /np`);
+		canary.sendText(`cd ${settings.getCanaryPath()}`);
+		canary.sendText(`./canary.exe`);
 
 		vscode.window.onDidCloseTerminal((closedTerminal) => {
-			if (closedTerminal === terminal) {
-				terminal = undefined;
+			if (closedTerminal === canary) {
+				canary = undefined;
 			}
 		});
 	}))
 
 	context.subscriptions.push(vscode.commands.registerCommand("run-login-mysql", () => {
-		if (!settings.getMySQLPath() || !settings.getLoginServerPath()) {
+		if (settings.getMySQLPath() === "" || settings.getLoginServerPath() === "") {
 			vscode.window.showErrorMessage('MySQL or login-server path is missing.');
+			return;
+		}
+
+		if (!fs.existsSync(`${settings.getLoginServerPath()}/login-server.exe`)) {
+			vscode.window.showErrorMessage('login-server.exe is missing.');
+			return;
+		}
+
+		if (!fs.existsSync(`${settings.getMySQLPath()}/mysqld_z.exe`)) {
+			vscode.window.showErrorMessage('mysqld_z.exe is missing.');
 			return;
 		}
 
